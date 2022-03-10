c462ec52a45a7fa8f8565aa07101bd26565c0fde
Ezy93 <giordani001@gmail.com>
2022-03-10 14:21:00 +0100

    aggiunta immagini e creazione array di oggetti

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
  *
  *
  *
-Riprendiamo l'esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice scritto oggi insieme a lezione, che troverete direttamente nella mia repository di github a questo link: [link github]
+Riprendiamo l'esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice scritto oggi insieme a lezione, 
+che troverete direttamente nella mia repository di github a questo link: [link github]
 
 Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto dei tre array separati, con una sola regola: non è possibile modificare l'HTML ma solamente JS e CSS.
 Ricordiamo sempre l'importanza dell'integrità del dato.
@@ -16,3 +17,44 @@ E se volessi un bottone per invertire la "direzione" del carosello?
 
  *
  */
+
+
+const landscape = [
+    {
+        nome : "Acadia National park",
+        descrizione : "vera e propria perla del New England, incastonato nella bellissima Mount Desert Island.",
+        image : "acadia-national-park.jpg",
+    },
+
+    {
+        nome : "Bar Harbor",
+        descrizione : "Bar Harbor è una città degli Stati Uniti d'America, situata nella contea di Hancock, nel Maine.",
+        image : "bar-harbor.jpg",
+    },
+
+    {
+        nome : "Costa oceano pacifico",
+        descrizione : "vera e propria perla dell'oceano pacifico.",
+        image : "coast.jpg",
+    },
+
+    {
+        nome : "Biedesheim",
+        descrizione : "Biedesheim è un comune di 634 abitanti della Renania-Palatinato, in Germania.",
+        image : "germany-biedesheim.jpg",
+    },
+
+    {
+        nome : "Scozia",
+        descrizione : "tipico paesaggio scozzese.",
+        image : "scotland.jpg",
+    },
+
+    {
+        nome : "Yellow stone National park",
+        descrizione : "non so se è davvero lo yellowstone.",
+        image : "yellowstone.jpg",
+    },
+];
+
+
