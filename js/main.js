c7c8912fabd4aeeb4ff646ffe2916ca51204a781
Ezy93 <giordani001@gmail.com>
2022-03-10 15:05:25 +0100

    costruzione dinamica thumbnail

--- js/main.js
+++ js/main.js
@@ -23,38 +23,50 @@ const landscape = [
     {
         nome : "Acadia National park",
         descrizione : "vera e propria perla del New England, incastonato nella bellissima Mount Desert Island.",
-        image : "acadia-national-park.jpg",
+        image : "img/acadia-national-park.jpg",
     },
 
     {
         nome : "Bar Harbor",
         descrizione : "Bar Harbor è una città degli Stati Uniti d'America, situata nella contea di Hancock, nel Maine.",
-        image : "bar-harbor.jpg",
+        image : "img/bar-harbor.jpg",
     },
 
     {
         nome : "Costa oceano pacifico",
         descrizione : "vera e propria perla dell'oceano pacifico.",
-        image : "coast.jpg",
+        image : "img/coast.jpg",
     },
 
     {
         nome : "Biedesheim",
         descrizione : "Biedesheim è un comune di 634 abitanti della Renania-Palatinato, in Germania.",
-        image : "germany-biedesheim.jpg",
+        image : "img/germany-biedesheim.jpg",
     },
 
     {
         nome : "Scozia",
         descrizione : "tipico paesaggio scozzese.",
-        image : "scotland.jpg",
+        image : "img/scotland.jpg",
     },
 
     {
         nome : "Yellow stone National park",
         descrizione : "non so se è davvero lo yellowstone.",
-        image : "yellowstone.jpg",
+        image : "img/yellowstone.jpg",
     },
 ];
 
+
+
+
+
+for(let i = 0; i < landscape.length; i++){
+    document.querySelector("div.my-thumbnails").innerHTML +=`
+    <div id= "thumb" style="width: calc(100% / ${landscape.length})"; >
+        <img class = "img-fluid" src="${landscape[i].image}" alt="${landscape[i].image}">
+    </div>`;
+    
+    
+}
 
